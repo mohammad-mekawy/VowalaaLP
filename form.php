db6105ad84f936c66a8f42f4fa9f82ea3e9c61ac
mohammad-mekawy <mohammad.mekawy92@gmail.com>
2022-09-15 09:00:57 +0200

    form validation

---
+++ form.php
@@ -0,0 +1,71 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Methods: GET,POST");
+// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['hero'])){
+    $name  = filter_var($_POST['name'],FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
+    $phone = filter_var($_POST['phone'],FILTER_SANITIZE_NUMBER_INT);
+    $COMPANY  = filter_var($_POST['COMPANY'],FILTER_SANITIZE_STRING);
+    $INDUSTRY  = filter_var($_POST['INDUSTRY'],FILTER_SANITIZE_STRING);
+
+    $formErrors = array();
+	
+    if(empty($name)){
+        $formErrors[] = "Add You name please";
+    }elseif(empty($phone)){
+        $formErrors[] = "Add You phone please";
+    }elseif(empty($email)){
+        $formErrors[] = "Add You email please";
+    }
+    if(empty($formErrors)){
+            $emails = array("m.mekkawy@happenize.com");
+			
+			for($i=0;$i<count($emails);$i++){
+				$mail = new PHPMailer;
+				$mail->CharSet = 'UTF-8';
+				$mail->SMTPDebug = 0;  
+				$mail->isSMTP();                            // Set mailer to use SMTP
+				$mail->Host = 'vowalaa.com';             // Specify main and backup SMTP servers
+				$mail->SMTPAuth = true;                     // Enable SMTP authentication
+				$mail->Username = 'lp@vowalaa.com';          // SMTP username
+				$mail->Password = '3htZ8o#PN6Qk'; // SMTP password
+				$mail->SMTPSecure = 'ssl';                            
+				$mail->Port = 465;  
+				$mail->isHTML(true);  // Set email format to HTML
+
+				$mail->setFrom('lp@vowalaa.com');
+				$mail->addAddress($emails[$i]); 
+
+				//$mail->isHTML(true);  // Set email format to HTML
+
+               $bodyContent = '<h3 style="font-size:18px; margin-bottom:12px;">Name: '. $name .'</h3><br>';
+               $bodyContent .= '<h3 style="font-size:18px; margin-bottom:12px;">Email: '. $email .'</h3><br>';
+               $bodyContent .= '<h3 style="font-size:18px; margin-bottom:12px;">Phone: '. $phone .'</h3><br>';
+               $bodyContent .= '<h3 style="font-size:18px; margin-bottom:12px;">COMPANY: '. $COMPANY .'</h3><br>';
+               $bodyContent .= '<h3 style="font-size:18px; margin-bottom:12px;">INDUSTRY: '. $INDUSTRY .'</h3><br>';
+
+               $mail->Subject = 'Vowalaa LP';
+               $mail->Body    = $bodyContent;
+            //    $mail->send();
+               if(!$mail->Send()) {
+                    echo "Error while sending Email.";
+                    var_dump($mail);
+                } else {
+                    // echo "Email sent successfully";
+                    header("Location: thanks.html");
+                    // exit;
+                }
+                
+			}
+    }
+}
+?>
