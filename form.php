7041886e993656c849bf32aeeecbe1d97dc27d4f
mohammad-mekawy <mohammad.mekawy92@gmail.com>
2022-09-21 12:39:53 +0200

    modules rearrange

--- form.php
+++ form.php
@@ -27,7 +27,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['hero'])){
         $formErrors[] = "Add You email please";
     }
     if(empty($formErrors)){
-            $emails = array("m.mekkawy@happenize.com");
+            $emails = array("mohammad.mekawy@happenize.com");
 			
 			for($i=0;$i<count($emails);$i++){
 				$mail = new PHPMailer;
@@ -62,6 +62,158 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['hero'])){
                 } else {
                     // echo "Email sent successfully";
                     header("Location: thanks.html");
+                    // exit;
+                }
+                
+			}
+    }
+}
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['sendemailCallNow'])){
+    $phone = filter_var($_POST['phone'],FILTER_SANITIZE_NUMBER_INT);
+
+    $formErrors = array();
+	
+    if(empty($phone)){
+        $formErrors[] = "Add You phone please";
+    }
+    if(empty($formErrors)){
+            $emails = array("mohammad.mekawy@happenize.com");
+			
+			for($i=0;$i<count($emails);$i++){
+				$mail = new PHPMailer;
+				$mail->CharSet = 'UTF-8';
+				$mail->SMTPDebug = 0;  
+				$mail->isSMTP();                            // Set mailer to use SMTP
+				$mail->Host = 'consultycs.com';             // Specify main and backup SMTP servers
+				$mail->SMTPAuth = true;                     // Enable SMTP authentication
+				$mail->Username = 'lp@vowalaa.com';          // SMTP username
+				$mail->Password = '3htZ8o#PN6Qk'; // SMTP password
+				$mail->SMTPSecure = 'ssl';                            
+				$mail->Port = 465;  
+				$mail->isHTML(true);  // Set email format to HTML
+
+				$mail->setFrom('lp@vowalaa.com');
+				$mail->addAddress($emails[$i]); 
+
+				//$mail->isHTML(true);  // Set email format to HTML
+
+               $bodyContent = '<h3 style="font-size:18px; margin-bottom:12px;">Phone: '. $phone .'</h3><br>';
+
+               $mail->Subject = 'Consultycs DMCC Dubai Lead';
+               $mail->Body    = $bodyContent;
+            //    $mail->send();
+               if(!$mail->Send()) {
+                    echo "Error while sending Email.";
+                    var_dump($mail);
+                } else {
+                    // echo "Email sent successfully";
+                    header("Location: thanks-call.html");
+                    // exit;
+                }
+                
+			}
+    }
+}
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['sendemailCallLater'])){
+    $date  = filter_var($_POST['date'],FILTER_SANITIZE_STRING);
+    $time = filter_var($_POST['time'],FILTER_SANITIZE_STRING);
+    $phone = filter_var($_POST['phone'],FILTER_SANITIZE_NUMBER_INT);
+
+    $formErrors = array();
+	
+    if(empty($date)){
+        $formErrors[] = "Choose date please";
+    }elseif(empty($phone)){
+        $formErrors[] = "Add You phone please";
+    }elseif(empty($time)){
+        $formErrors[] = "Choose time please";
+    }
+    if(empty($formErrors)){
+            $emails = array("mohammad.mekawy@happenize.com");
+			
+			for($i=0;$i<count($emails);$i++){
+				$mail = new PHPMailer;
+				$mail->CharSet = 'UTF-8';
+				$mail->SMTPDebug = 0;  
+				$mail->isSMTP();                            // Set mailer to use SMTP
+				$mail->Host = 'consultycs.com';             // Specify main and backup SMTP servers
+				$mail->SMTPAuth = true;                     // Enable SMTP authentication
+				$mail->Username = 'lp@vowalaa.com';          // SMTP username
+				$mail->Password = '3htZ8o#PN6Qk'; // SMTP password
+				$mail->SMTPSecure = 'ssl';                            
+				$mail->Port = 465;  
+				$mail->isHTML(true);  // Set email format to HTML
+
+				$mail->setFrom('lp@vowalaa.com');
+				$mail->addAddress($emails[$i]); 
+
+				//$mail->isHTML(true);  // Set email format to HTML
+
+               $bodyContent = '<h3 style="font-size:18px; margin-bottom:12px;">Name: '. $date .'</h3><br>';
+               $bodyContent .= '<h3 style="font-size:18px; margin-bottom:12px;">Email: '. $time .'</h3><br>';
+               $bodyContent .= '<h3 style="font-size:18px; margin-bottom:12px;">Phone: '. $phone .'</h3><br>';
+
+               $mail->Subject = 'Consultycs DMCC Dubai Lead';
+               $mail->Body    = $bodyContent;
+            //    $mail->send();
+               if(!$mail->Send()) {
+                    echo "Error while sending Email.";
+                    var_dump($mail);
+                } else {
+                    // echo "Email sent successfully";
+                    header("Location: thanks-call.html");
+                    // exit;
+                }
+                
+			}
+    }
+}
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['sendemailLeaveMessage'])){
+    $email = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
+    $phone = filter_var($_POST['phone'],FILTER_SANITIZE_NUMBER_INT);
+    $message  = filter_var($_POST['message'],FILTER_SANITIZE_STRING);
+
+    $formErrors = array();
+	
+    if(empty($phone)){
+        $formErrors[] = "Add You phone please";
+    }elseif(empty($email)){
+        $formErrors[] = "Add You email please";
+    }
+    if(empty($formErrors)){
+            $emails = array("mohammad.mekawy@happenize.com");
+			
+			for($i=0;$i<count($emails);$i++){
+				$mail = new PHPMailer;
+				$mail->CharSet = 'UTF-8';
+				$mail->SMTPDebug = 0;  
+				$mail->isSMTP();                            // Set mailer to use SMTP
+				$mail->Host = 'consultycs.com';             // Specify main and backup SMTP servers
+				$mail->SMTPAuth = true;                     // Enable SMTP authentication
+				$mail->Username = 'lp@vowalaa.com';          // SMTP username
+				$mail->Password = '3htZ8o#PN6Qk'; // SMTP password
+				$mail->SMTPSecure = 'ssl';                            
+				$mail->Port = 465;  
+				$mail->isHTML(true);  // Set email format to HTML
+
+				$mail->setFrom('lp@vowalaa.com');
+				$mail->addAddress($emails[$i]); 
+
+				//$mail->isHTML(true);  // Set email format to HTML
+
+               $bodyContent = '<h3 style="font-size:18px; margin-bottom:12px;">Phone: '. $phone .'</h3><br>';
+               $bodyContent .= '<h3 style="font-size:18px; margin-bottom:12px;">Email: '. $email .'</h3><br>';
+               $bodyContent .= '<h3 style="font-size:18px; margin-bottom:12px;">Message: '. $message .'</h3><br>';
+
+               $mail->Subject = 'Consultycs DMCC Dubai Lead';
+               $mail->Body    = $bodyContent;
+            //    $mail->send();
+               if(!$mail->Send()) {
+                    echo "Error while sending Email.";
+                    var_dump($mail);
+                } else {
+                    // echo "Email sent successfully";
+                    header("Location: thanks-call.html");
                     // exit;
                 }
                 
